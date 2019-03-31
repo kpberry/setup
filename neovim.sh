9451ebc41b6679c9632f3752b195cfeb1deb274d
Kevin Berry <kpberry11@gmail.com>
2019-03-31 16:58:33 -0400

    Fixes vim installation

--- neovim.sh
+++ neovim.sh
@@ -2,4 +2,6 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 sudo pip3 install pynvim
-# vim +PlugInstall +qall
+mkdir -p ~/.config/nvim/
+cp init.vim ~/.config/nvim/init.vim
+vim +PlugInstall +qall
