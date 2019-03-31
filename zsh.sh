36ed53df297facc44d242d90b9a9d3c4741a52bf
Kevin Berry <kpberry11@gmail.com>
2019-03-31 17:02:17 -0400

    Fixes zsh installation

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,5 @@
 sudo apt-get install zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-sudo chsh -s `which zsh`
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
