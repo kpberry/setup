25b3815e7f0a91ddbfcb902f3b2362d25bcc1c41
kpberry <kpberry11@gmail.com>
2019-04-18 12:01:26 -0400

    Adds plug installation to neovim.sh

--- neovim.sh
+++ neovim.sh
@@ -2,6 +2,8 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 sudo pip3 install pynvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.config/nvim/
 cp init.vim ~/.config/nvim/init.vim
 vim +PlugInstall +qall
