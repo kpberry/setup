a8876182beecf6c15a73c17ba6468fa77d83f347
kpberry <kpberry11@gmail.com>
2019-04-18 12:11:51 -0400

    Adds .zshrc copy to zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -3,3 +3,4 @@ wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - 
 chsh -s $(which zsh)
 sudo chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp .zshrc ~
