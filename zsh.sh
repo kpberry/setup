5028e060b6894a49b2c074be19a064346e622a64
Kevin Berry <kpberry11@gmail.com>
2019-06-16 15:01:44 -0400

    Updates zsh for pacman

--- zsh.sh
+++ zsh.sh
@@ -1,6 +1,6 @@
-sudo apt-get install zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+sudo pacman -S zsh
 chsh -s $(which zsh)
 sudo chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp .zshrc ~
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
