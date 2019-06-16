bc160693683d4ab3f3e9d7cb82512620a288316c
Kevin Berry <kpberry11@gmail.com>
2019-06-16 15:28:15 -0400

    Cleans up clone destinations

--- misc.sh
+++ misc.sh
@@ -1,6 +1,6 @@
 sudo pacman -S tmux htop musescore graphviz gimp zip tree
 
-git clone https://aur.archlinux.org/qdirstat.git /tmp/qdirstat
-cd /tmp/qdirstat
+git clone https://aur.archlinux.org/qdirstat.git /tmp/qdirstat/
+cd /tmp/qdirstat/
 makepkg -si
 cd -
--- neovim.sh
+++ neovim.sh
@@ -3,8 +3,8 @@ sudo ln -s /usr/bin/vim /usr/bin/nvim
 mkdir -p ~/.config/nvim/
 cp init.vim ~/.config/nvim/init.vim
 
-git clone https://aur.archlinux.org/neovim-plug.git /tmp/neovim-plug
-cd /tmp/neovim-plug
+git clone https://aur.archlinux.org/neovim-plug.git /tmp/neovim-plug/
+cd /tmp/neovim-plug/
 makepkg -si
 cd -
 vim +PlugInstall +qall
