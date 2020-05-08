a34d07e39145e98733ac2ddfdcb9068ae6e24b7f
Kevin Berry <kpberry@pop-os.localdomain>
2020-05-08 00:23:38 -0400

    Changes docker.sh for ubuntu 20

--- docker.sh
+++ docker.sh
@@ -1,7 +1,4 @@
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt install docker.io
+sudo systemctl enable --now docker
+sudo usermod -aG docker $(whoami)
 
